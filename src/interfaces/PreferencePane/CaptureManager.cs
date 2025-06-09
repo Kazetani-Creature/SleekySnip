675b3f1cd435e5a91709e5d6d71848a1794d8f1c
Tamiko Yotsuba <63150734+YotsubaTamiko@users.noreply.github.com>
2025-06-09 12:46:34 +0800

    Document listener usage

--- src/interfaces/PreferencePane/CaptureManager.cs
+++ src/interfaces/PreferencePane/CaptureManager.cs
@@ -1,36 +1,15 @@
 using Microsoft.UI.Xaml;
-using System.Windows.Forms;
 using Windows.System;
 using SleekySnip.Core;
 using System.IO;
+using Llsvc.KeyboardListener;
 
 namespace PreferencePane;
 
 public static class CaptureManager
 {
     private static CaptureWindow? _window;
-    private static HotKeyRegistrar? _registrar;
-    private static MessageWindow? _messageWindow;
-
-    private const int WM_HOTKEY = 0x0312;
-
-    private sealed class MessageWindow : NativeWindow
-    {
-        public MessageWindow()
-        {
-            CreateHandle(new CreateParams());
-        }
-
-        protected override void WndProc(ref Message m)
-        {
-            if (m.Msg == WM_HOTKEY)
-            {
-                ShowFlyout();
-            }
-
-            base.WndProc(ref m);
-        }
-    }
+    private static KeyboardListener? _listener;
 
     public static void ShowFlyout()
     {
@@ -45,10 +24,8 @@ public static class CaptureManager
 
     public static void Initialize()
     {
-        if (_messageWindow != null)
+        if (_listener != null)
             return;
-
-        _messageWindow = new MessageWindow();
 
         var settingsPath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "resources", "SleekySnip.props");
         var settings = SleekySnip.Core.SleekySnipSettingsSerializer.Load(settingsPath);
@@ -56,17 +33,17 @@ public static class CaptureManager
         VirtualKey key = VirtualKey.Snapshot;
         Enum.TryParse(settings.Hotkey, out key);
 
-        _registrar = new SleekySnip.Core.HotKeyRegistrar(_messageWindow.Handle);
-        _registrar.TryRegister(0, (uint)key);
+        _listener = new KeyboardListener();
+        _listener.SetProcessCommand(id => ShowFlyout());
+        _listener.SetHotkeyAction(win: false, ctrl: false, shift: false, alt: false, key: (byte)key, id: "capture");
+        _listener.Start();
 
         Application.Current.Exit += OnAppExit;
     }
 
     private static void OnAppExit(object sender, object e)
     {
-        _registrar?.Dispose();
-        _registrar = null;
-        _messageWindow?.DestroyHandle();
-        _messageWindow = null;
+        _listener?.Dispose();
+        _listener = null;
     }
 }
