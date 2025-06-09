f713c0a3c3db996226281773ec23d2c2dda583c1
Tamiko Yotsuba <63150734+YotsubaTamiko@users.noreply.github.com>
2025-06-09 11:53:43 +0800

    Add global hotkey registration

--- src/interfaces/PreferencePane/CaptureManager.cs
+++ src/interfaces/PreferencePane/CaptureManager.cs
@@ -1,10 +1,36 @@
 using Microsoft.UI.Xaml;
+using System.Windows.Forms;
+using Windows.System;
+using SleekySnip.Core;
+using System.IO;
 
 namespace PreferencePane;
 
 public static class CaptureManager
 {
     private static CaptureWindow? _window;
+    private static HotKeyRegistrar? _registrar;
+    private static MessageWindow? _messageWindow;
+
+    private const int WM_HOTKEY = 0x0312;
+
+    private sealed class MessageWindow : NativeWindow
+    {
+        public MessageWindow()
+        {
+            CreateHandle(new CreateParams());
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_HOTKEY)
+            {
+                ShowFlyout();
+            }
+
+            base.WndProc(ref m);
+        }
+    }
 
     public static void ShowFlyout()
     {
@@ -19,6 +45,28 @@ public static class CaptureManager
 
     public static void Initialize()
     {
-        // TODO: register global hotkey and call ShowFlyout when triggered
+        if (_messageWindow != null)
+            return;
+
+        _messageWindow = new MessageWindow();
+
+        var settingsPath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "resources", "SleekySnip.props");
+        var settings = SleekySnip.Core.SleekySnipSettingsSerializer.Load(settingsPath);
+
+        VirtualKey key = VirtualKey.Snapshot;
+        Enum.TryParse(settings.Hotkey, out key);
+
+        _registrar = new SleekySnip.Core.HotKeyRegistrar(_messageWindow.Handle);
+        _registrar.TryRegister(0, (uint)key);
+
+        Application.Current.Exit += OnAppExit;
+    }
+
+    private static void OnAppExit(object sender, object e)
+    {
+        _registrar?.Dispose();
+        _registrar = null;
+        _messageWindow?.DestroyHandle();
+        _messageWindow = null;
     }
 }
